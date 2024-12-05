3192db60d207cd1d96f810276f8a04c5d6d2a180
cbdhoho <81568368+cbdhoho@users.noreply.github.com>
2024-12-05 11:23:04 +0900

    fix: both playing issue fixed

--- white-noise-script.js
+++ white-noise-script.js
@@ -3,16 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentSongTitle = document.getElementById('currentSongTitle');
     const musicList = document.getElementById('music-list').children;
 
-    // Set up click events for music list items
     Array.from(musicList).forEach(item => {
         item.addEventListener('click', () => {
             const songSrc = item.getAttribute('data-src');
             const songTitle = item.textContent;
+            
+            // 현재 재생 중인 노래를 정지
+            if (!audioPlayer.paused) {
+                audioPlayer.pause();
+                audioPlayer.currentTime = 0; // 재생 위치 초기화                
+            }
 
+            // 새로운 노래 로드 및 재생
             audioPlayer.src = songSrc;
-            audioPlayer.play();
+            audioPlayer.play().catch(error => {
+                console.error(`Error playing the song: ${error.message}`);
+            });
 
+            // 현재 재생 중인 노래 제목 업데이트
             currentSongTitle.textContent = `재생 중인 노래: ${songTitle}`;
         });
     });
+    
 });
