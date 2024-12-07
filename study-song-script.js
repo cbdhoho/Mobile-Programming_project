a1f66279f9e0b1fa02c38aa1e4e145a2c0be222f
cbdhoho <81568368+cbdhoho@users.noreply.github.com>
2024-12-08 00:48:16 +0900

    refactor: study-song

--- study-song-script.js
+++ study-song-script.js
@@ -7,22 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             const songSrc = item.getAttribute('data-src');
             const songTitle = item.textContent;
-            
-            // 현재 재생 중인 노래를 정지
+
             if (!audioPlayer.paused) {
                 audioPlayer.pause();
-                audioPlayer.currentTime = 0; // 재생 위치 초기화                
+                audioPlayer.currentTime = 0;
             }
 
-            // 새로운 노래 로드 및 재생
             audioPlayer.src = songSrc;
             audioPlayer.play().catch(error => {
                 console.error(`Error playing the song: ${error.message}`);
             });
 
-            // 현재 재생 중인 노래 제목 업데이트
             currentSongTitle.textContent = `재생 중인 노래: ${songTitle}`;
         });
     });
-    
 });
